177e4834718786809c51d9bcd7b35584a4a7994f
Nandeenee Singh <nandeenee.singh@gmail.com>
2026-03-16 22:35:53 -0400

    Used astroquery logc

--- Orbital-ForwardModel/OrbitalCleaningDataScript.py
+++ Orbital-ForwardModel/OrbitalCleaningDataScript.py
@@ -1,31 +1,29 @@
 import pandas as pd
 import numpy as np
+from astroquery.gaia import Gaia
 
-# Load Gaia Orbital Solutions data
-df = pd.read_csv('data/nss_orbital_results_2.csv')
+# Define ADQL query 
+query = """
+SELECT
+    source_id, ra, dec, parallax, significance, period, 
+    ruwe, a_thiele_innes, b_thiele_innes 
+FROM gaiadr3.nss_acceleration_astrometric
+WHERE parallax >0
+AND significance > 10
+AND period < 1000
+AND ruwe > 1.4
+AND ruwe < 4.0
+"""
 
-# 1. Remove rows of missing or unrealistic parallax values
-df = df[df['parallax'] > 0]
+# Execute the query and retrieve the data
+job = Gaia.launch_job(query)
+results = job.get_results()
 
-# 2. Filter for high-confidence wobbles
-# Scientists use > 5 for binaries, but > 10 for planets. Let's use > 10 to account for any weirdly shaped stars or other sources of noise that could be mistaken for a wobble.
-df = df[df['significance'] > 10]
+# Convert results to a pandas DataFrame
+df = results.to_pandas()
 
-# 3. Filter by orbital period 
-# Gaia DR3's baseline is around 34 months, so we can only reliably detect planets with orbital periods shorter than that. Let's set a cutoff at 1000 days to be safe.
-df = df[df['period'] < 1000]
-
-# 4. A RUWE filter will flag unresolved binaries, which can produce false positives. Let's set a cutoff at 1.4, which is commonly used in the Gaia community.
-# We want stars that wobble, but aren't so weird that we can't trust the data.
-df = df[(df['ruwe'] > 1.4) & (df['ruwe'] < 4.0)]
-
-
-
-# --- Forward modeling to estimate planet masses ---
-
-# Calculate Wobble Amplitude (alpha) in arcseconds
-# Alpha = sqrt(a^2 + b^2) where a and b are the Thiele-Innes constants from Gaia's orbital solutions
+# Forward Model to estimate the mass of the companion
 df['wobble_amplitude'] = np.sqrt(df['a_thiele_innes']**2 + df['b_thiele_innes']**2)
 
-# Save cleaned data to a new CSV file
-df.to_csv('data/cleaned_nss_orbital_results.csv', index=False)
+# Save result 
+df.to_csv('data/cleaned_gaia_data.csv', index=False)
